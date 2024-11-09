fe67dc26e6aac93f5c90487a89ce539972ef470a
Ali <m.ali.rezaeiii09@gmail.com>
2024-11-09 17:54:04 +0330

    Initial Commit

---
+++ StandfordAssignment/StandfordAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandfordAssignmentApp.swift
+//  StandfordAssignment
+//
+//  Created by Ali on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct StandfordAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
